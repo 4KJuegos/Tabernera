329771923436e0edb5b364afd28b8a0513bf0761
4KJuegos <51173839+4KJuegos@users.noreply.github.com>
2019-05-29 21:01:18 +0200

    Update and rename config.js.example to config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	token: 'NTgzMzY1MzI2MjY0MjA1MzI4.XO7VIA.8ac5bZTrQEl7ffk6SrpmA5oJvWQ',
+	ownerID: '583365326264205328',
+	mysql: {
+		host: 'ec2-54-247-96-169.eu-west-1.compute.amazonaws.com',
+		user: 'byspdotefutjrc',
+		password: '913edfe25856b98a6723c9e449ba3e889f9e0f365c7785e7c8a6a168876ac613',
+		database: 'dcefauoeocq78b'
+	}
+};
